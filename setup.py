f49fada0cbfd5e9a619e0a78095b2ca8201bc569
Morteza-Khazaei <mortezakhazaei1370@gmail.com>
2023-08-24 16:07:59 +0200

    add command line interface

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     install_requires = requirements,
     entry_points ={
         'console_scripts': [
-            'start_maja = StartMaja.src.core:main'
+            'StartMaja = StartMaja.core:main'
         ]
     },
     classifiers=[
